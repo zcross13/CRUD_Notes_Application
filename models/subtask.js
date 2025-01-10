00717efead51d154bede5113ad9c6885f21b31cd
zcross13 <zalicross24@gmail.com>
2025-01-09 18:53:38 -0600

    add subtask model

---
+++ models/subtask.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+const subTaskSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: false,
+        maxLength: 200,
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'In Progress', 'Completed', 'Delayed'],
+        default: 'Pending'
+    },
+    priority: {
+        type: String,
+        enum: ['Low', 'Medium', 'High'],
+        default: 'Medium'
+    },
+    taskId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Task',
+        required: true,
+    },
+    dueDate: {
+        type: Date
+    },
+    assignedTo: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, { timestamps: true })
+
+module.exports = model('SubTask', subTaskSchema)
